c794e8d30920931422cec522cb2c033f9952be80
Huiman-Diego <134320980+Huiman-Diego@users.noreply.github.com>
2023-07-31 17:47:10 +0000

    repository

--- src/main/java/com/example/Semana11Repaso/EmployeeRepository.java
+++ src/main/java/com/example/Semana11Repaso/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.example.Semana11Repaso;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "empleados", path = "emps")
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
