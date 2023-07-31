666e29d74f23e2bd2fd6b98988a4b7033f9140ef
Huiman-Diego <134320980+Huiman-Diego@users.noreply.github.com>
2023-07-31 17:09:22 +0000

    repos

---
+++ src/main/java/com/example/Semana11Repaso/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.Semana11Repaso;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
