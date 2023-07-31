6dbbe741242ed8e9c2e7ebd9cda2b8548e80b6b1
Huiman-Diego <134320980+Huiman-Diego@users.noreply.github.com>
2023-07-31 17:04:59 +0000

    repaso1

---
+++ src/main/java/com/example/Semana11Repaso/Semana11RepasoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Semana11Repaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana11RepasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana11RepasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Semana11Repaso/Semana11RepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Semana11Repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana11RepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
